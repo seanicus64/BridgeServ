32e385bfa6b1000a2bdb301148a068a815ce49ea
Ubuntu <topdev@TopDev.bhtmn0qkzytu3bm4wmq3viengh.gx.internal.cloudapp.net>
2018-01-08 05:27:48 +0000

    rewrote discord bot so it won't crash

--- bots/discord/bot.py
+++ bots/discord/bot.py
@@ -1,98 +1,155 @@
 #!/usr/bin/env python3
+# Proof of concept to use discord API and twisted simulteanously.
+# this token obviously won't work.
+token = "token"
+import json
 import discord
 import asyncio
-import socket
-import threading
-import json
-import configparser
+import random
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import ClientFactory
+from twisted.internet import asyncioreactor
+from twisted.protocols.basic import LineReceiver
+from twisted.internet.defer import Deferred
+# For some reason, you must install an asyncioreactor before
+# you import reactor.
+myloop = asyncio.get_event_loop()
+asyncioreactor.install(myloop)
+from twisted.internet import reactor
+
+# This must be defined before all the coroutines are used.
 client = discord.Client()
 
-class BridgeConnection:
-    def __init__(self, socket, bridge_pass):
-        self.socket = socket
-        self.receive = threading.Thread(target=self.get_data)
-        self.receive.start()
-                            
-        self.authenticate(bridge_pass)
-    def authenticate(self, bridge_pass):
+class MyProtocol(LineReceiver):
+    """Client to an arbitrary server.  Sends and receives arbitary data."""
+    def __init__(self, factory):
+        self.factory = factory
+    def connectionMade(self):
+        """ Runs when connection is made to server."""
         data = {}
         data["command"] = "authenticate"
-        data["password"] = bridge_pass
-        data = json.dumps(data)
-        self.send(data)
-    def get_data(self):
-        """ recieves data from the bridge, converts it to usable commands"""
-        print("hello")
-        i = 0
-        while True:
-            i += 1
-            print(i)
-            data = self.socket.recv(4096)
-#            print(data)
-#            if not data:
-#                continue
-#            print(data)
-#            try:
-#                data = json.load(data.decode("utf-8"))
-#            except JSONDecodeError:
-#                continue
-#            print(data)
-    def register_user(self, nick, username, hostname, realname):
+        data["password"] = self.factory.settings["DEFAULT"]["bridge_password"]
+        data = json.dumps(data).encode("utf-8")
+        self.sendLine(data)
+        print("connection made to bridge")
+        
+    def lineReceived(self, line):
+        print("line received:", line)
+    def register_user(self, nick, user, host, real):
         data = {}
         data["command"] = "register"
         data["nick"] = nick
-        data["username"] = username
-        data["hostname"] = hostname
-        data["realname"] = realname
-        data = json.dumps(data)
-        self.send(data)
-    def privmsg(self, name, target, message):
-        data = {}
-        data["command"] = "privmsg"
-        data["nick"] = name
-        data["destination"] = target
-        data["message"] = message
-        data = json.dumps(data)
-        self.send(data)
-    def join(self, name, channel):
+        data["username"] = user
+        data["hostname"] = host
+        data["realname"] = real
+        self.send_message(data)
+    def join(self, nick, channel):
         data = {}
         data["command"] = "join"
-        data["nick"] = name
+        data["nick"] = nick
         data["channel"] = channel
-        data = json.dumps(data)
-        self.send(data)
-    def send(self, message):
-        message = bytes((message + "\n").encode("utf-8"))
-        self.socket.send(message)
-@client.event
-async def on_ready():
-    """When the client firsts connects to discord."""
-    print(client.get_all_members())
-    members = client.get_all_members()
-    for m in members:
-        print(m.display_name)
-        nick = m.display_name.replace(" ", "_") #+ "|"
-        print(nick)
-        bridge_connection.register_user(nick, "discord", "bridge", "abcdefg")
-        bridge_connection.join(nick, "#banana")
-@client.event
-async def on_message(message):
-    """What happens when a message is recieved on IRC."""
-    print(message.author)
-    print(message.author.display_name)
-    print(message.content)
-    target = "#banana"
-    nick = message.author.display_name.replace(" ", "_")# + "|"
-    print(nick)
-    bridge_connection.privmsg(nick, target, message.content)
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.send_message(data)
+    def send_message(self, data):
+        """This will send a message to the server."""
+        message = bytes(json.dumps(data).encode("utf-8"))
+        self.sendLine(message)
+        print("I sent out:\t{}".format(message))
+    def privmsg(self, nick, recipient, message):
+        data = {}
+        data["command"] = "privmsg"
+        data["nick"] = nick
+        data["destination"] = recipient
+        data["message"] = message
+        self.send_message(data)
+
+class MyFactory(ClientFactory):
+    """Necessary to create a protocol.  This is where all the
+        permanent data is supposed to be stored."""
+    def __init__(self, settings):
+        self.protocol = None
+        self.settings = settings
+        self.users_to_nicks = {}
+        self.users = {}
+        self.synced = False
+        self.to_be_resolved = {}
+    def connected_to_discord(self, client):
+        """
+        This creates a deferred as soon as you connect to 
+        Discord, so when you actually connect to the bridge
+        you can actually fire the callback chain."""
+        self.client = client
+        self.sync_deferred = Deferred()
+        self.sync_deferred.addCallback(self.register_list)
+    def sync_up(self):
+        members = list(self.client.get_all_members())
+        self.sync_deferred.callback(members)
+
+
+    def attempt_register(self, user, nick):
+        self.to_be_resolved[user.id] = nick 
+        self.protocol.register_user(nick, "user", "host", "real")
+    
+    def register_list(self, users):
+        for user in users:
+            print(user)
+            nick = user.display_name.replace(" ", "_") + "_"
+            # temporary guarantee that nick isnt on network yet.  TODO: make better
+            nick += str(random.randrange(100000)).zfill(5)
+            self.attempt_register(user, nick)
+            print(nick)
+            self.protocol.join(nick, "#banana")
+            
+    def send_to_protocol(self, message):
+        """ Simply relays a message to the server we are talking to,"""
+        if self.protocol:
+            self.protocol.send_message(message.encode("utf-8"))
+    def buildProtocol(self, addr):
+        """Actually creates the protocol, when ran with reactor.run()"""
+        self.protocol = MyProtocol(self)
+        return self.protocol
+    def new_message(self, message):
+        author = message.author
+        discord_id = message.author.id
+        nick = self.to_be_resolved[discord_id]
+        recipient = "#banana"
+        self.protocol.privmsg(nick, recipient, message.content)
+
+
+# Creating this factory before the coroutines because send_to_protocol
+# could theoretically be run before connection is established.
+import configparser
 config = configparser.ConfigParser()
 config.read("bot.conf")
-server = config["DEFAULT"]["host"]
-port = int(config["DEFAULT"]["port"])
-bridge_pass = config["DEFAULT"]["bridge_password"]
 token = config["DEFAULT"]["discord_token"]
-socket.connect((server, port))
-socket.setblocking(1)
-bridge_connection = BridgeConnection(socket, bridge_pass)
-client.run(token)
+my_factory = MyFactory(config)
+
+
+@client.event
+async def on_ready():
+    """Everything that happens when the discord bot connects initially."""
+    print("Logged in as: {}".format(client.user.name))
+    print("we are connected to discord")
+    my_factory.connected_to_discord(client)
+    my_factory.sync_up()
+
+@client.event
+async def on_message(message):
+    """Everything that happens when a message is sent on the discord server."""
+    print(dir(message))
+    print(message.author)
+    my_factory.new_message(message)
+#    my_factory.send_to_protocol(message.content)    
+# honestly not sure how to describe this but
+# just read this: http://discordpy.readthedocs.io/en/latest/migrating.html#running-the-client
+@asyncio.coroutine
+def main_task():
+    yield from client.login(token)
+    yield from client.connect()
+# The asyncio loop was already added to the reactor.  Now we add the main_task coroutine
+# which will actually run the bot.
+asyncio.ensure_future(main_task())
+# We can then run the reacor normally, knowing it will also run the event loop for asyncio
+#reactor.connectTCP("irc.freenode.net", 6667, my_factory)
+
+reactor.connectTCP("localhost", 5959, my_factory)
+reactor.run()
